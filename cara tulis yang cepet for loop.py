5a313febdff4ac7b2b0bf7fb34a8acf6b67f4bdf
cenovaxs <67851104+cenovaxs@users.noreply.github.com>
2020-08-03 20:37:57 +0700

    Buang lambda
    pusing soalnya

--- cara tulis yang cepet for loop.py
+++ cara tulis yang cepet for loop.py
@@ -1,5 +1,5 @@
-nums = [1,2,3,4,5,6,7,8,9]
-my_list=[n*n for n in nums]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+my_list = [n*n for n in nums if n % 2 == 0]
 counter = 0
 max_index = len(nums)
 my_list2 = []
@@ -8,6 +8,6 @@ while counter < max_index:
     counter = counter + 1
     if num % 2 == 0:
         my_list2.append(num*num)
-my_list3 = list(map(lambda n: n*n if x % 2 == 0, nums))
-print(my_list3)
-print(my_list2 == my_list == my_list3)
+
+print(my_list2)
+print(my_list == my_list2)
