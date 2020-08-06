6bc3dc45f94a7b7a42bbd967ab8474958d9cc87a
cenovaxs <67851104+cenovaxs@users.noreply.github.com>
2020-08-06 10:59:47 +0700

    final tim version

--- tictactoe.py
+++ tictactoe.py
@@ -1,45 +1,42 @@
-# This should be the first line in your program
+#Tic Tac Toe Game Python
+
 board = [' ' for x in range(10)]
-# board is now: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 
 def insertBoard(letter, pos):
+    global board
     board[pos] = letter
 
 
 def spaceIsFree(pos):
     return board[pos] == ' '
-# This function will return a True or False value
 
 
 def isWinner(bo, le):
     # Given a board and a player’s letter, this function returns True if that player has won.
     # We use bo instead of board and le instead of letter so we don’t have to type as much.
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
-            (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
-            (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
-            (bo[7] == le and bo[4] == le and bo[1] == le) or  # down the left side
-            (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
-            # down the right side
-            (bo[9] == le and bo[6] == le and bo[3] == le) or
-            (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
-            (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
+    (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
+    (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
+    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
+    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
+    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
+    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
+    (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
 
 
 def playerMove():
-    def playerMove():
     run = True
-    while run:  # Keep looping until we get a valid move
-        move = input('Please select a position to place an \'X\' (1-9): ')
+    while run:
+        move = input('Please select a position to place an "X" (1-9): ')
         try:
-            move = int(move)
-            if move > 0 and move < 10:  # makes sure we type in a number between 1-9
-                # check if the move we choose is valid (no other letter is there already)
+            move  = int(move)
+            if move > 0 and move < 10:
                 if spaceIsFree(move):
                     run = False
                     insertBoard('X', move)
                 else:
-                    print('This position is already occupied!')
+                    print('This postion is already occupied!')
             else:
                 print('Please type a number within the range!')
         except:
@@ -47,16 +44,54 @@ def playerMove():
 
 
 def selectRandom(li):
-    pass
+    import random
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
 
 
 def compMove():
-    pass
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    #Check for possible winning move to take or to block opponents winning move
+    for let in ['O','X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+
+
+    #Try to take one of the corners
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    #Try to take the center
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    #Take any edge
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+
+    return move
 
 
 def isBoardFull(board):
-    def isBoardFull(board):
-    if board.count(' ') > 1:  # Since we always have one blank element in board we must use > 1
+    if board.count(' ') > 1:
         return False
     else:
         return True
@@ -64,22 +99,23 @@ def isBoardFull(board):
 
 def printBoard():
     # "board" is a list of 10 strings representing the board (ignore index 0)
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
+    print(' -------------')
 
+    print(' | ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ')
+    
+    print(' -------------')
+    
+    print(' | ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' | ')
+    
+    print(' -------------')
+    
+    print(' | ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' | ')
+
+    print(' -------------')
+    
 
 def main():
-    def main():
-        # Main game loop
+    #Main game loop
     print('Welcome to Tic Tac Toe, to win complete a straight line of your letter (Diagonal, Horizontal, Vertical). The board has positions 1-9 starting at the top left.')
     printBoard()
 
@@ -88,7 +124,7 @@ def main():
             playerMove()
             printBoard()
         else:
-            print('O\'s win this time...')
+            print('Os win this time...')
             break
 
         if not(isWinner(board, 'X')):
@@ -97,11 +133,23 @@ def main():
                 print('Game is a Tie! No more spaces left to move.')
             else:
                 insertBoard('O', move)
-                print('Computer placed an \'O\' in position', move, ':')
+                print("Computer placed an 'O' in position", move, ":")
                 printBoard()
         else:
-            print('X\'s win, good job!')
+            print('Xs win, good job!')
             break
+
 
     if isBoardFull(board):
         print('Game is a tie! No more spaces left to move.')
+
+main()
+
+while True:
+    answer = input('Do you want to play again? (Y/N)')
+    if answer.lower() == 'y' or answer.lower == 'yes':
+        board = [' ' for x in range(10)]
+        print('-----------------------------------')
+        main()
+    else:
+        break
